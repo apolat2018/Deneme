7b393d2d8eb79d6744b89d721a1f2c70b623ee25
apolat2018 <39853484+apolat2018@users.noreply.github.com>
2018-06-02 12:46:47 +0300

    Add files via upload

--- deneme.py
+++ deneme.py
@@ -1,3 +1,9 @@
 #Github için merhaba dünya yazdýran program
 # -*- coding: cp1254 -*-
 print "Merhaba Dünya"
+print "Ali POLAT-2018"
+for i in range(10):
+    if i%2==0:
+        print i,"sayýsý ikiye tam bölünür"
+    else:
+        pass
